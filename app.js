d1d287710e8d53400a747c97e7867918bd49a4e2
armorangel <aa007650@gmail.com>
2019-05-17 00:56:30 +0000

    최초 작성

---
+++ app.js
@@ -0,0 +1,30 @@
+console.log(11111);
+
+
+
+/*
+var file = require('fs');  // 파일시스템 관련 기본 모듈
+var util = require('util');  // 유틸리티 함수를 제공하는 기본 모듈
+*/
+
+var express = require('express');//웹 서버 사용.
+var app = express();
+var fs = require('fs');//파일 로드 사용
+
+//포트 설정
+app.listen(9000, function(){
+ console.log('Server Start');
+});
+
+
+//라우팅 설정
+app.get('/', function(req,res){//웹 서버 기본주소로 접속 할 경우 실행.
+	fs.readFile('index.html', function(error, data){//test.html 파일 로드.
+		if(error){
+			console.log(error);
+		} else {
+			res.writeHead(200,{'Content-Type':'text/html'});//Head Type설정.
+			res.end(data);//파일 로드 html response.
+		}
+	}); 
+});
