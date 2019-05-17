b86783645d7ce2c1232174f5ba5ec0eff84a1e46
armorangel <aa007650@gmail.com>
2019-05-17 01:00:20 +0000

    최초 작성

---
+++ server.js
@@ -0,0 +1,11 @@
+// server.js
+ /*
+var server = require('http');
+ 
+server.createServer(function(req, res) {
+	res.writeHead(200, {'Content-Type':'text/plain'});
+	res.end('Hello World\n');
+}).listen(3000, 'localhost');
+ 
+console.log('Server is started...');
+*/
