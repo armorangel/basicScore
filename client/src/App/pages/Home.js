3a300b7062cc11926c39f63a0752a882c3bc0cd7
armorangel <armorangel@naver.com>
2019-05-23 08:40:35 +0000

    test

--- client/src/App/pages/Home.js
+++ client/src/App/pages/Home.js
@@ -6,7 +6,7 @@ class Home extends Component {
   render() {
     return (
     <div className="App">
-      <h1>Project Home</h1>
+      <h1>Project Home!!</h1>
       {/* Link to List.js */}
       <Link to={'./list'}>
         <button variant="raised">
