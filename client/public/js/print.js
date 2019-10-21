31a434ed61b534b30eff2d615c261cad601b7ef1
armorangel <armorangel@naver.com>
2019-10-21 07:41:22 +0000

    ...

--- client/public/js/print.js
+++ client/public/js/print.js
@@ -422,16 +422,16 @@ kara.keySVG = function(Y, key, trcNm) {	// 217, major Db, track1
 //박자 SVG
 kara.meterSVG = function(Y, trcNm) {// trcNm :: Track Name - 'track1'
 	
-	let box = kara.svg[trcNm].svgBox;
-	let key = kara.scoreInfo.key;	// 'major Db'
-	let keySplit = key.split(' ');
-	let M = kara.key[keySplit[0]];	// A: 3, Ab: 4, B: 5, Bb: 2, C: 0, D: 2, Db: 5, E: 4, Eb: 3, F: 1, G: 1, Gb: 6
-	let N = M[keySplit[1]];	// 5
+	let box = kara.svg[trcNm].svgBox
+	let key = kara.scoreInfo.key	// 'major Db'
+	let keySplit = key.split(' ')
+	let M = kara.key[keySplit[0]]	// A: 3, Ab: 4, B: 5, Bb: 2, C: 0, D: 2, Db: 5, E: 4, Eb: 3, F: 1, G: 1, Gb: 6
+	let N = M[keySplit[1]]	// 5
 
-	let x, y, width, height;
+	let x, y, width, height
 
-	let meter = kara.scoreInfo.meter;	// 악보정보 객체 박자
-	let meterSplit = meter.split('/');
+	let meter = kara.scoreInfo.meter	// 악보정보 객체 박자
+	let meterSplit = meter.split('/')
 
 	var pathString = this.pathClone(kara.glyphs[meterSplit[0]].d, 45 + (N * 10), Y / 1.4 + 24.7);
 	var pathString2 = this.pathClone(kara.glyphs[meterSplit[1]].d, 45 + (N * 10), Y / 1.4 + 41.7);
