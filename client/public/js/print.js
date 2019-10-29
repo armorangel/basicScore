001743c71d6ee2a746f7cffdb72b6a37b4e1a636
armorangel <armorangel@naver.com>
2019-10-29 07:55:49 +0000

    ...

--- client/public/js/print.js
+++ client/public/js/print.js
@@ -467,17 +467,20 @@ kara.meterSVG = function(Y, trcNm) {// trcNm :: Track Name - 'track1'
 //꼬리 SVG
 kara.printflag = function(symb, x, y, trcNm) {	// flags.uquarter, track1, 141.15625381469727, 235.99998474121094
 	
-	let svg = kara.svg[trcNm].svgSymbol
+	// let svg = kara.svg[trcNm].svgSymbol
 	
 	if (!kara.glyphs[symb]) return null;
 	
 	let pathString = this.pathClone(kara.glyphs[symb].d, x + 10.35, y - 29)
 	
-	// 합치자
+	kara.draw.symbol(trcNm, pathString)
+	
+	/*
 	svg.append('path')
 		.attr('class', kara.conf.del + ' ' + trcNm)	// 삭제 영역
 		.attr('d', pathString)
 		.style('stroke', 'black')
+	*/
 }
 
 // Draw Sharp, Flat, Rest, head
